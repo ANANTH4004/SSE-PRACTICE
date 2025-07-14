603a2015575b53ae525d31364ec295a5ba40e385
ANANTH4004 <balamuruganananth@gmail.com>
2025-07-14 08:38:24 +0530

    Accordian refactor

--- DOM/Accordian/reacc.js
+++ DOM/Accordian/reacc.js
@@ -1,4 +1,4 @@
-class Accordian extends HTMLElement {
+class AccordianRe extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
@@ -6,62 +6,55 @@ class Accordian extends HTMLElement {
     const wrapper = document.createElement('div')
     wrapper.classList.add('accordian')
 
-    //create a header
     const header = document.createElement('div')
     header.classList.add('accordian-header')
     header.textContent = this.getAttribute('title') || 'Accordian'
 
-    //create a content
     const content = document.createElement('div')
     content.classList.add('accordian-content')
 
     const slot = document.createElement('slot')
     content.appendChild(slot)
 
-    wrapper.appendChild(header)
-    wrapper.appendChild(content)
-
     const style = document.createElement('style')
-    style.textContent = `
-      .accordian {
+    style.textContent = `.accordian {
         border: 1px solid #ccc;
         margin: 10px 0;
         border-radius: 5px;
-        font-family: sans-serif;
-        overflow: hidden;
-      }
-      .accordian-header {
+        }
+        .accordian-header {
         background: #f2f2f2;
         padding: 10px 15px;
         cursor: pointer;
         font-weight: bold;
-       
       }
-      .accordian-header::after {
+
+      .accordian-header::after{
         content: '+';
-        position: absolute;
-        right: 25px;
+        position:absolute;
+        right: 20px;
       }
-      :host(.open) .accordian-header::after {
+    :host(.open) .accordian-header::after{
         content: '-';
-      }
-      .accordian-content {
+    }
+
+    .accordian-content{
         max-height: 0;
+        transition: max-height 0.9s ease;
         overflow: hidden;
-        transition: max-height 0.5s ease;
-        padding: 0 15px;
-      }
-
-    `
-
+        padding: 0 10px;
+    }
+        :host(.open) .accordian-content {
+   
+}
+        `
+    wrapper.appendChild(header)
+    wrapper.appendChild(content)
     this.shadowRoot.append(style, wrapper)
-
     header.addEventListener('click', () => {
       const isOpen = this.classList.toggle('open')
       content.style.maxHeight = isOpen ? content.scrollHeight + 'px' : null
-      null
     })
-
     this.contentEl = content
   }
   connectedCallback() {
@@ -69,4 +62,4 @@ class Accordian extends HTMLElement {
   }
 }
 
-customElements.define('accordian-el', Accordian)
+customElements.define('re-accor', AccordianRe)
